5b0737920cf397edcc57a8f54eef10bf6384f159
danielpclin <daniel.pclin@gmail.com>
2023-11-22 00:41:10 +0800

    update

--- lua/danielpclin/treesitter-setup.lua
+++ lua/danielpclin/treesitter-setup.lua
@@ -32,6 +32,10 @@ vim.defer_fn(function()
           ['if'] = '@function.inner',
           ['ac'] = '@class.outer',
           ['ic'] = '@class.inner',
+          ['al'] = '@loop.outer',
+          ['il'] = '@loop.inner',
+          ['ai'] = '@conditional.outer',
+          ['ii'] = '@conditional.inner',
         },
       },
       move = {
