64c065426acd1ece9041cf894d1b2107e0c85679
danielpclin <daniel.pclin@gmail.com>
2023-11-22 00:16:41 +0800

    update

--- lua/danielpclin/mini-setup.lua
+++ lua/danielpclin/mini-setup.lua
@@ -3,5 +3,6 @@ require('mini.move').setup({})
 require('mini.trailspace').setup({})
 require('mini.pairs').setup({})
 require('mini.surround').setup({})
+require('mini.cursorword').setup({})
 
 -- vim: ts=2 sts=2 sw=2 et
