0606520c24ea678f701cb3ea85a1f6ea53f53354
danielpclin <daniel.pclin@gmail.com>
2023-11-22 00:53:42 +0800

    update

--- lua/danielpclin/which-keys-setup.lua
+++ lua/danielpclin/which-keys-setup.lua
@@ -9,6 +9,9 @@ require('which-key').register {
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>v'] = { name = '[V]iew', _ = 'which_key_ignore' },
+  ['<leader>vc'] = { name = '[V]iew [C]ode', _ = 'which_key_ignore' },
+  ['<leader>vd'] = { name = '[V]iew [D]ocument', _ = 'which_key_ignore' },
+  ['<leader>vw'] = { name = '[V]iew [W]orkspace', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
   ['<leader>x'] = { name = 'Troubleshoot', _ = 'which_key_ignore' },
 }
